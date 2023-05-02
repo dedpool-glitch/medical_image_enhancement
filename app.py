054ba52489a2d71ce32d9e42b515b127cf122f47
Nalin Rajput <63943978+dedpool-glitch@users.noreply.github.com>
2023-05-02 17:06:41 +0530

    final versions of app and notebook

--- app.py
+++ app.py
@@ -1,30 +1,52 @@
-import streamlit as st
+from flask import Flask, request, send_file
+from PIL import Image
+import io
 import cv2
-import polars as pl
+import numpy as np
 
-def process_image(image):
-    grey_img=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    return """
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Image Processing</title>
+</head>
+<body>
+  <h1>Image Processing</h1>
+  <form action="process_image" method="post" enctype="multipart/form-data">
+    <input type="file" name="image_file" accept="image/*"><br><br>
+    <input type="submit" value="Process">
+  </form>
+</body>
+</html>
+"""
+
+@app.route('/process_image', methods=['POST'])
+def process_image():
+    img_bytes = request.files['image_file'].read()
+    image = Image.open(io.BytesIO(img_bytes))
+    img = np.array(image)
     clahe=cv2.createCLAHE(clipLimit=3.0,tileGridSize=(5,5))
+    grey_img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     clahe_img=clahe.apply(grey_img)
-    return clahe_img
+    
+    
+    # Process the image using your Python function
+    processed_image =Image.fromarray(np.uint8(clahe_img))
+    processed_image.resize((300,300),resample=0)
 
-def app():
-    st.title("Medical Image Enhancement")
-
-    # Allow the user to upload an image file
-    uploaded_file = st.file_uploader("Choose an image file", type=["jpg", "jpeg", "png"])
-
-    # If an image file is uploaded, display it and process it
-    if uploaded_file is not None:
-        # Read the image file
-        image = cv2.imread(uploaded_file.name)
-        # Process the image
-        processed_image = process_image(image)
-
-        # Display the input and output images
-        st.image(image, caption="Input Image", use_column_width=True)
-        st.image(processed_image, caption="Output Image after applying CLAHE", use_column_width=True)
+    
+    # Save the output image to a buffer
+    output_buffer = io.BytesIO()
+    processed_image.save(output_buffer, format='PNG')
+    output_buffer.seek(0)
+    
+    return send_file(output_buffer, mimetype='image/png')
 
 if __name__ == '__main__':
-    app()
+    app.run(debug=True)
+
 
